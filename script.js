991ec9fb43904a079d866304efa32e07e8899365
Apolavste <bebemj997@gmail.com>
2024-05-10 13:08:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,4 @@
 /* 이 JavaScript 파일은 민지 포트폴리오 페이지의 동적인 기능을 구현합니다. */
 /* 작성자: 민지 */
 
- var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        direction: 'horizontal',
-        freeMode: true,
-        paginationClickable: true,
-        spaceBetween: 0,
-        mousewheelControl: true,
-        parallax: true,
-        speed: 600,
 
-    });
