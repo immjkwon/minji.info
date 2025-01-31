fd3b38be3a75b21e411e89df91e5474a2329f6ff
immjkwon <bebemj997@gmail.com>
2025-01-31 14:42:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 /* 작성자: 민지 */
 
 
+    document.addEventListener('scroll', function () {
+        const introduction = document.getElementById('introduction');
+        const introParagraphs = introduction.querySelectorAll('p');
+
+        introParagraphs.forEach(p => {
+            const rect = p.getBoundingClientRect();
+            if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+                p.classList.add('visible');
+            }
+        });
+    });
